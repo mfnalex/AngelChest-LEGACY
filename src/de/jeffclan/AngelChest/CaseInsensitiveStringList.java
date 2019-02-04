2e69f9120103f2a43cb2b44ab79b866090cba01b
BuildTools <unconfigured@null.spigotmc.org>
2019-02-04 15:19:40 +0100

    .

---
+++ src/de/jeffclan/AngelChest/CaseInsensitiveStringList.java
@@ -0,0 +1,19 @@
+package de.jeffclan.AngelChest;
+
+import java.util.ArrayList;
+
+class CaseInsensitiveStringList extends ArrayList<String> {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7754120806084460569L;
+
+	@Override
+    public boolean contains(Object o) {
+        String paramStr = (String)o;
+        for (String s : this) {
+            if (paramStr.equalsIgnoreCase(s)) return true;
+        }
+        return false;
+    }
+}
